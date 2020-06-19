6d04f02804517300451bd2b8ab904a1b8dfbf0ea
Mert <certel@hotmail.com>
2020-06-20 00:49:05 +0300

    social icons added

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,36 @@
 import React from "react"
-
+import { Gitlab, Twitter, LinkedIn } from "./social"
 function footer() {
   return (
     <footer className="footer">
-      <div class="content has-text-centered">
-        <p className="is-size-4 ">
-          You can say{" "}
-          <a
-            href="mailto:certel@hotmail.com"
-            target="_blank"
-            className="box-shadow-red say-hello"
-          >
-            hello
-          </a>{" "}
-          to me below or you can{" "}
-          <span className="box-shadow-green">hire me</span>
-        </p>
+      <div className="colums">
+        <div class="column content has-text-centered">
+          <p className="is-size-4 ">
+            You can say{" "}
+            <a
+              href="mailto:certel@hotmail.com"
+              target="_blank"
+              className="box-shadow-red say-hello"
+            >
+              hello
+            </a>{" "}
+            to me below or you can{" "}
+            <span className="box-shadow-green">hire me.</span>
+          </p>
+        </div>
+        <div className="colum ">
+          <div className="footer-social">
+            <a target="_blank" href="https://gitlab.com/itoldyou/">
+              <Gitlab />
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/in/mcertel/">
+              <LinkedIn />
+            </a>{" "}
+            <a href="https://twitter.com/certel1st" target="_blank">
+              <Twitter />
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   )
